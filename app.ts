d809003e2a4e5f141c403718d17a7b8551c3420d
jisakd1 <kagan.dupuy.reagan@jbhunt.com>
2021-06-27 18:45:00 -0500

    Call openlibrary api for each book isbn in table and print response body

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+var mysql = require('mysql'); 
 interface Book {
     id: number,
     title: string,
@@ -13,15 +13,31 @@ var con = mysql.createConnection({
     password: "password",
     database: "book_inventory"
   });
-console.log(con);
-con.connect(function(err: any) {
+var http = require('http');
+
+const pathTemplate = '/api/books?bibkeys=ISBN:{isbn-placeholder}&jscmd=details&format=json';
+const options = {
+  host: 'www.openlibrary.org',
+  path: ''
+};
+con.connect(function(err: Error) {
     if (err) throw err;
     console.log("Connected!");
     const sql = "select * from books;";
     con.query(sql, function (err: any, result: Array<Book>) {
         if (err) throw err;
-        result.forEach(book => {
-            console.log("Result: " + JSON.stringify(book));
+        result.forEach(async book => {
+            //console.log("Result: " + JSON.stringify(book));
+            var path = pathTemplate.replace('{isbn-placeholder}',book.isbn.toString());
+            options.path = path;
+            console.log(options.host + options.path);
+            var callback = function(response: any) {
+              response.on('data', function (chunk: string) {
+                console.log('BODY: ' + chunk);
+              });
+            };
+            http.request(options, callback).end();
         });
       });
+    con.end();
 });
